2736afc514255fe44cd03033ee93ac73552af9df
Audrey <diep.audrey@gmail.com>
2023-04-01 18:51:10 +0200

    add camera & input

--- TP6_sphere/main.cpp
+++ TP6_sphere/main.cpp
@@ -1,11 +1,15 @@
+
 #include "cstddef"
+#include "glimac/Trackball.hpp"
 #include "glimac/common.hpp"
+#include "glimac/cone_vertices.hpp"
 #include "glimac/default_shader.hpp"
 #include "glimac/sphere_vertices.hpp"
 #include "glm/ext/matrix_clip_space.hpp"
 #include "glm/ext/matrix_transform.hpp"
 #include "glm/glm.hpp"
 #include "glm/gtc/type_ptr.hpp"
+#include "input.hpp"
 #include "p6/p6.h"
 
 int main()
@@ -13,54 +17,60 @@ int main()
     auto ctx = p6::Context{{1280, 720, "TP3 EX1"}};
     ctx.maximize_window();
 
+    /*Camera*/
+    glimac::TrackballCamera camera;
+    float                   movementStrength = 10.f;
+
+    // not working
+    // Input input(camera, movementStrength);
+
+    ctx.mouse_dragged = [&camera, &movementStrength](p6::MouseDrag data) {
+        data.delta = data.position - data.start_position;
+
+        camera.rotateLeft(data.delta.x * movementStrength);
+        camera.rotateUp(-data.delta.y * movementStrength);
+    };
+
+    ctx.mouse_scrolled = [&camera](p6::MouseScroll data) {
+        camera.moveFront(data.dy);
+    };
+
+    ctx.key_repeated = [&camera, &ctx, &movementStrength](const p6::Key& data) {
+        if (data.logical == "z" || data.logical == "Z")
+        {
+            camera.moveFront(ctx.delta_time() * movementStrength);
+        }
+
+        if (data.logical == "s" || data.logical == "S")
+        {
+            camera.moveFront(-ctx.delta_time() * movementStrength);
+        }
+    };
+
     /*VBO*/
     GLuint vbo;
     glGenBuffers(1, &vbo);
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    std::vector<glimac::ShapeVertex> vertices = glimac::sphere_vertices(1, 32, 16);
-
-    // std::cout << "Vertices size = " << vertices.size() << std::endl;
-    // std::cout << "Vertices data = " << vertices.data() << std::endl;
+    std::vector<glimac::ShapeVertex> vertices = glimac::cone_vertices(1.f, 0.5f, 5, 5);
 
     glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(glimac::ShapeVertex), vertices.data(), GL_STATIC_DRAW);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
 
     /*VAO*/
-    // GLuint vao;
-    // glGenVertexArrays(1, &vao);
-    // glBindVertexArray(vao);
-
-    // static constexpr GLuint vertex_attr_position = 0;
-    // glEnableVertexAttribArray(vertex_attr_position);
-
-    // glBindBuffer(GL_ARRAY_BUFFER, vbo);
-
-    // glVertexAttribPointer(vertex_attr_position, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(GLfloat), 0);
-
-    // glBindBuffer(GL_ARRAY_BUFFER, 0);
-    // glBindVertexArray(0);
-
     GLuint vao;
-
     glGenVertexArrays(1, &vao);
-
     glBindVertexArray(vao);
 
-    static constexpr GLuint VERTEX_ATTR_POSITION = 0;
-
-    static constexpr GLuint VERTEX_ATTR_NORMAL = 1;
-
+    static constexpr GLuint VERTEX_ATTR_POSITION  = 0;
+    static constexpr GLuint VERTEX_ATTR_NORMAL    = 1;
     static constexpr GLuint VERTEX_ATTR_TEXCOORDS = 2;
-
     glEnableVertexAttribArray(VERTEX_ATTR_POSITION);
-
     glEnableVertexAttribArray(VERTEX_ATTR_NORMAL);
-
     glEnableVertexAttribArray(VERTEX_ATTR_TEXCOORDS);
 
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
 
-    glVertexAttribPointer(VERTEX_ATTR_POSITION, 3, GL_FLOAT, GL_FALSE, sizeof(glimac::ShapeVertex), (const GLvoid*)(offsetof(glimac::ShapeVertex, position)));
+    glVertexAttribPointer(VERTEX_ATTR_POSITION, 3, GL_FLOAT, GL_FALSE, sizeof(glimac::ShapeVertex), (const GLvoid*)nullptr);
 
     glVertexAttribPointer(VERTEX_ATTR_NORMAL, 3, GL_FLOAT, GL_FALSE, sizeof(glimac::ShapeVertex), (const GLvoid*)(offsetof(glimac::ShapeVertex, normal)));
 
@@ -84,8 +94,10 @@ int main()
 
     // Declare your infinite update loop.
     ctx.update = [&]() {
+        glm::mat4 viewMatrix = camera.getViewMatrix();
+
         glm::mat4 ProjMatrix   = glm::perspective(glm::radians(70.f), ctx.aspect_ratio(), 0.1f, 100.f);
-        glm::mat4 MVMatrix     = glm::translate(glm::mat4(1.0f), glm::vec3(0.f, 0.f, -5.0f));
+        glm::mat4 MVMatrix     = glm::translate(glm::mat4(1.f), glm::vec3(0.f, 0.f, -5.0f));
         glm::mat4 NormalMatrix = glm::transpose(glm::inverse(MVMatrix));
 
         glimac::bind_default_shader();
@@ -93,7 +105,7 @@ int main()
 
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        glUniformMatrix4fv(uMVPMatrix, 1, GL_FALSE, glm::value_ptr(ProjMatrix * MVMatrix));
+        glUniformMatrix4fv(uMVPMatrix, 1, GL_FALSE, glm::value_ptr(ProjMatrix * viewMatrix));
 
         glUniformMatrix4fv(uMVMatrix, 1, GL_FALSE, glm::value_ptr(MVMatrix));
 
@@ -102,6 +114,7 @@ int main()
         glBindVertexArray(vao);
 
         glDrawArrays(GL_TRIANGLES, 0, vertices.size());
+
         glBindVertexArray(0);
     };
 
---
+++ src-common/glimac/Trackball.hpp
@@ -0,0 +1,67 @@
+#pragma once
+
+#include "glm/ext/matrix_clip_space.hpp"
+#include "glm/ext/matrix_transform.hpp"
+#include "glm/glm.hpp"
+
+namespace glimac {
+
+class TrackballCamera {
+private:
+    float m_fDistance;
+    float m_fAngleX;
+    float m_fAngleY;
+
+public:
+    // constructeur
+    TrackballCamera()
+        : m_fDistance(5.f), m_fAngleX(0.f), m_fAngleY(0.f){};
+
+    TrackballCamera(const float dist, const float angleX, const float angleY)
+        : m_fDistance(dist), m_fAngleX(angleX), m_fAngleY(angleY){};
+
+    // getter/setter
+    float getDistance()
+    {
+        return this->m_fDistance;
+    }
+    float getAngleX()
+    {
+        return this->m_fAngleX;
+    }
+    float getAngleY()
+    {
+        return this->m_fAngleY;
+    }
+    void setDistance(const float dist)
+    {
+        this->m_fDistance = dist;
+    }
+
+    // movements
+    void moveFront(const float delta)
+    {
+        this->m_fDistance += -delta;
+    };
+
+    void rotateLeft(const float degrees)
+    {
+        this->m_fAngleY += degrees;
+    }
+
+    void rotateUp(const float degrees)
+    {
+        this->m_fAngleX += degrees;
+    }
+
+    glm::mat4 getViewMatrix() const
+    {
+        glm::mat4 viewMatrix = glm::translate(glm::mat4(1.f), glm::vec3(0.f, 0.f, -(this->m_fDistance)));
+        viewMatrix           = glm::rotate(viewMatrix, glm::radians(this->m_fAngleX), glm::vec3(1.0f, 0.f, 0.f));
+        viewMatrix           = glm::rotate(viewMatrix, glm::radians(this->m_fAngleY), glm::vec3(0.f, 1.0f, 0.f));
+
+        return viewMatrix;
+    }
+};
+
+} // namespace glimac
